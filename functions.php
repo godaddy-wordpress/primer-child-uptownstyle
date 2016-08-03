aa8e41b8cbc1d835142e6bf87ec1a9ca40a78beb
Matt Pritchett <mpritchett@crowdfavorite.com>
2016-08-03 16:06:52 -0400

    Updating theme hero.

--- functions.php
+++ functions.php
@@ -65,31 +65,6 @@ function uptown_move_navigation() {
 add_action( 'primer_header', 'uptown_move_navigation', 19 );
 
 /**
- * Returns the featured image, custom header or false in this priority order.
- *
- * @return false|string
- */
-function uptown_get_custom_header() {
-	$post_id = get_queried_object_id();
-	$image_size = (int) get_theme_mod( 'full_width' ) === 1 ? 'hero-2x' : 'hero';
-	if ( has_post_thumbnail( $post_id ) ) {
-		$image = get_the_post_thumbnail_url( $post_id, $image_size );
-		if ( getimagesize( $image ) ) {
-			return $image;
-		}
-	}
-	$custom_header = get_custom_header();
-	if ( ! empty( $custom_header->attachment_id ) ) {
-		$image = wp_get_attachment_image_url( $custom_header->attachment_id, $image_size );
-		if ( getimagesize( $image ) ) {
-			return $image;
-		}
-	}
-	$header_image = get_header_image();
-	return $header_image;
-}
-
-/**
  *
  * Register Hero Widget.
  *
