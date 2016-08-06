0885614444e92b376de77b79c92dcba3f3a62b29
Chris Wallace <chris@liftux.com>
2016-08-05 21:01:57 -0500

    Fix hero background not displaying on non-homepage

--- templates/parts/hero.php
+++ templates/parts/hero.php
@@ -5,7 +5,7 @@
  * @package Uptown Style
  */
 ?>
-<div class="hero" <?php if ( ! empty( uptown_get_header_image() ) && is_front_page() ) : ?> style="background:url('<?php echo uptown_get_header_image( ); ?>') no-repeat top center; background-size: cover;"<?php endif; ?>>
+<div class="hero" <?php if ( ! empty( uptown_get_header_image() ) ) : ?> style="background:url('<?php echo uptown_get_header_image( ); ?>') no-repeat top center; background-size: cover;"<?php endif; ?>>
 
 	<div class="hero-wrapper">
 
