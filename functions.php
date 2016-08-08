b1f2ab65ee933218b984f90989dec3a49e3b7aef
Chris Wallace <chris@liftux.com>
2016-08-08 16:32:54 -0500

    Make function prefixes consistent.

--- functions.php
+++ functions.php
@@ -97,7 +97,7 @@ add_action( 'primer_header', 'uptown_move_navigation', 19 );
  * @link  http://codex.wordpress.org/Function_Reference/register_sidebar
  * @since 1.0.0
  */
-function uptown_style_register_sidebars() {
+function uptown_register_sidebars() {
 
 	/**
 	 *
@@ -118,7 +118,7 @@ function uptown_style_register_sidebars() {
 	);
 
 }
-add_action( 'widgets_init', 'uptown_style_register_sidebars' );
+add_action( 'widgets_init', 'uptown_register_sidebars' );
 
 
 /**
@@ -127,12 +127,12 @@ add_action( 'widgets_init', 'uptown_style_register_sidebars' );
  * @package Uptown Style
  * @link https://codex.wordpress.org/Function_Reference/add_image_size
  */
-function uptown_style_add_image_size() {
+function uptown_add_image_size() {
 
 	add_image_size( 'hero', 2400, 1320, array( 'center', 'center' ) );
 
 }
-add_action( 'after_setup_theme', 'uptown_style_add_image_size' );
+add_action( 'after_setup_theme', 'uptown_add_image_size' );
 
 /**
  * Update custom header arguments
@@ -141,13 +141,13 @@ add_action( 'after_setup_theme', 'uptown_style_add_image_size' );
  * @param $args
  * @return mixed
  */
-function uptown_style_update_custom_header_args( $args ) {
+function uptown_update_custom_header_args( $args ) {
 	$args['width'] = 2400;
 	$args['height'] = 1320;
 
 	return $args;
 }
-add_filter( 'primer_custom_header_args', 'uptown_style_update_custom_header_args' );
+add_filter( 'primer_custom_header_args', 'uptown_update_custom_header_args' );
 
 /**
  * Add hero after header if we are on a post or front page.
@@ -156,13 +156,13 @@ add_filter( 'primer_custom_header_args', 'uptown_style_update_custom_header_args
  * @action primer_after_header
  * @since 1.0.0
  */
-function uptown_style_add_hero() {
+function uptown_add_hero() {
 
 	remove_action( 'primer_header', 'primer_add_hero', 10 );
 	add_action( 'primer_after_header', 'primer_add_hero', 20 );
 
 }
-add_action( 'after_setup_theme', 'uptown_style_add_hero' );
+add_action( 'after_setup_theme', 'uptown_add_hero' );
 
 /**
  * Get header image with image size
@@ -170,7 +170,7 @@ add_action( 'after_setup_theme', 'uptown_style_add_hero' );
  * @package Uptown Style
  * @return false|string
  */
-function uptown_style_get_header_image() {
+function uptown_get_header_image() {
 	$image_size = (int) get_theme_mod( 'full_width' ) === 1 ? 'hero-2x' : 'hero';
 	$custom_header = get_custom_header();
 
@@ -190,7 +190,7 @@ function uptown_style_get_header_image() {
  * @package Uptown Style
  * @action primer_colors
  */
-function uptown_style_colors() {
+function uptown_colors() {
 	return array(
 		array(
 			'name'    => 'link_color',
@@ -253,7 +253,7 @@ function uptown_style_colors() {
 		),
 	);
 }
-add_action( 'primer_colors', 'uptown_style_colors', 9 );
+add_action( 'primer_colors', 'uptown_colors', 9 );
 
 /**
  * Change uptown color schemes
@@ -263,7 +263,7 @@ add_action( 'primer_colors', 'uptown_style_colors', 9 );
  * @since 1.0.0
  * @return array
  */
-function uptown_style_color_schemes() {
+function uptown_color_schemes() {
 	return array(
 		'bronze' => array(
 			'label'  => esc_html__( 'Bronze', 'uptown_style' ),
@@ -278,7 +278,7 @@ function uptown_style_color_schemes() {
 		),
 	);
 }
-add_action( 'primer_color_schemes', 'uptown_style_color_schemes' );
+add_action( 'primer_color_schemes', 'uptown_color_schemes' );
 
 /**
  *
@@ -287,7 +287,7 @@ add_action( 'primer_color_schemes', 'uptown_style_color_schemes' );
  * @package Uptown Style
  * @since 1.0.0
  */
-function uptown_style_update_font_types() {
+function uptown_update_font_types() {
 	return	array(
 		array(
 			'name'    => 'primary_font',
@@ -311,7 +311,7 @@ function uptown_style_update_font_types() {
 		),
 	);
 }
-add_action( 'primer_font_types', 'uptown_style_update_font_types', 5 );
+add_action( 'primer_font_types', 'uptown_update_font_types', 5 );
 
 /**
  *
@@ -320,9 +320,9 @@ add_action( 'primer_font_types', 'uptown_style_update_font_types', 5 );
  * @package Uptown Style
  * @since 1.0.0
  */
-function uptown_style_add_default_header_image( $array ) {
+function uptown_add_default_header_image( $array ) {
 	$array['default-image'] = get_stylesheet_directory_uri() . '/assets/img/header.jpg';
 
 	return $array;
 }
-add_filter( 'primer_custom_header_args', 'uptown_style_add_default_header_image', 20 );
+add_filter( 'primer_custom_header_args', 'uptown_add_default_header_image', 20 );
